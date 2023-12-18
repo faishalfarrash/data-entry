be537886effb09b375788f6592c9c8d9b48c9165
faishalfarrash <faishalfarrash97@gmail.com>
2023-12-18 23:34:05 +0700

    Form

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,7 +2,6 @@ export default function Home() {
     return (
         <div className="wrapper">
             <div className="selection">
-
                 <div>
                     <div className="flex flex-row gap-5 p-5 justify-between">
                         <div className="text-3xl font-bold">
@@ -16,30 +15,74 @@ export default function Home() {
                     <div className="p-2">
                         <table>
                             <tr>
+                                <th> NOMOR </th>
+                                <th> TANGGAL </th>
                                 <th className="border-black"> NOMOR SURAT </th>
-                                <th> KODE DIVISI </th>
-                                <th> PENGIRIM INSTANSI </th>
-                                <th> ALAMAT </th>
-                                <th> PENERIMA INSTANSI </th>
-                                <th> PERIHAL </th>
                                 <th> SIFAT SURAT </th>
-                                <th> Edit </th>
-                                <th> Delete </th>
+                                <th> PERIHAL </th>
+                                <th> DARI </th>
+                                <th> TUJUAN </th>
+                                <th> ACTION </th>
                             </tr>
                             <tr>
-                                <td> Alfreds Futterkiste </td>
-                                <td> Maria Anders </td>
-                                <td> Germany </td>
-                                <td> Placeholder </td>
-                                <td> Placeholder </td>
-                                <td> Placeholder </td>
-                                <td> Placeholder </td>
-                                <td> Placeholder </td>
-                                <td> Placeholder </td>
+                                <td> 1 </td>
+                                <td> 22-11-2023 </td>
+                                <td> 070/3555-kesbangpol.poldagri </td>
+                                <td>  </td>
+                                <td> Permohonan PKL a.n Ananda Dastra  </td>
+                                <td> Kesbangpol </td>
+                                <td> Kesbaormas </td>
+                                <td> Edit Delete</td>
+                            </tr>
+                            <tr>
+                                <td> 2 </td>
+                                <td> 22-11-2023 </td>
+                                <td> 070/3555-kesbangpol.poldagri </td>
+                                <td>  </td>
+                                <td> Permohonan PKL a.n Ananda Dastra  </td>
+                                <td> Kesbangpol </td>
+                                <td> Kesbaormas </td>
+                                <td> Edit Delete</td>
                             </tr>
                         </table>
                     </div>
                 </div>
+            </div>
+            <div>
+                <form className="wrapper2" action="/action_page.php">
+                    <div style={{ display: "flex", flexDirection: "row" }}>
+                        <div for="nsurat" style={{ width: "120px" }}> Nomor Surat : </div>
+                        <input type="text" id="nsurat" name="fname" value="" className="input" />
+                    </div>
+                    <div style={{ display: "flex", flexDirection: "row" }}>
+                        <div for="lname" style={{ width: "120px" }}> Sifat Surat : </div>
+                        <div style={{display:"flex", gap:"20px"}}>
+                            <div>
+                                <input type="checkbox" id="vehicle1" name="vehicle1" value="biasa" />
+                                <label for="vehicle1"> Biasa </label>
+                            </div>
+                            <div>
+                                <input type="checkbox" id="vehicle2" name="vehicle2" value="rahasia" />
+                                <label for="vehicle2"> Rahasia </label>
+                            </div>
+                        </div>
+                    </div>
+                    <div style={{ display: "flex", flexDirection: "row" }}>
+                        <div for="lname" style={{ width: "120px" }}> Perihal : </div>
+                        <input type="text" id="lname" name="lname" value="" className="input" />
+                    </div>
+                    <div style={{ display: "flex", flexDirection: "row" }}>
+                        <div for="dari" style={{width:"120px"}}> Dari : </div>
+                        <input type="text" id="dari" name="dari" value="" className="input" />
+                    </div>
+                    <div style={{ display: "flex", flexDirection: "row" }}>
+                        <div for="tujuan" style={{width:"120px"}}> Tujuan : </div>
+                        <input type="text" id="tujuan" name="tujuan" value="" className="input" />
+                    </div>
+                    <div>
+                        <input type="submit" value="Submit" className="submit" />
+                    </div>
+                </form>
             </div>
         </div>
     )
