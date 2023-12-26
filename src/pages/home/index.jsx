ec80353006e9610ef463fae1a7f3c202a434c660
faishalfarrash <faishalfarrash97@gmail.com>
2023-12-26 23:34:20 +0700

    Modals

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,15 +1,6 @@
-import { useState } from "react";
-
-const bob = [
-    {
-        date: "22-12-2023",
-        letter_number: "070/3555-kesbangpol.poldagri",
-        sifat: true,
-        perihal: "Permohonan",
-        dari: "Tadi",
-        tujuan: "Kesbang",
-    }
-]
+import { useState, Fragment } from "react";
+import { Dialog, Transition } from '@headlessui/react'
+import dayjs from "dayjs";
 
 export default function Home() {
     const [nomorSurat, setNomorSurat] = useState();
@@ -18,10 +9,11 @@ export default function Home() {
     const [perihal, setPerihal] = useState();
     const [dari, setDari] = useState();
     const [tujuan, setTujuan] = useState();
+    const [isOpen, setIsOpen] = useState(false)
 
     const handleSubmit = () => {
         const inputData = {
-            date: "22-12-2023",
+            date: dayjs().format('DD/MM/YYYY (HH:mm)'),
             letter_number: nomorSurat,
             sifat: sifatSurat,
             perihal,
@@ -31,6 +23,17 @@ export default function Home() {
 
         const updated = [...data, inputData];
         localStorage.setItem("data", JSON.stringify(updated));
+        setIsOpen(false)
+        window.location.reload()
+    }
+
+
+    function closeModal() {
+        setIsOpen(false)
+    }
+
+    function openModal() {
+        setIsOpen(true)
     }
 
     return (
@@ -43,13 +46,13 @@ export default function Home() {
                         </div>
                         <div className="flex flex-row justify-end gap-5 ">
                             <input className="rounded-lg border-black border p-2" placeholder="Cari..." />
-                            <button className="bg-gray-800 rounded-lg p-2 text-white"> TAMBAH </button>
+                            <button className="bg-gray-800 rounded-lg p-2 text-white" onClick={openModal}> TAMBAH </button>
                         </div>
                     </div>
                     <div className="p-2">
                         <table>
                             <tr>
-                                <th> NOMOR </th>
+                                <th> NO </th>
                                 <th> TANGGAL </th>
                                 <th className="border-black"> NOMOR SURAT </th>
                                 <th> SIFAT SURAT </th>
@@ -76,40 +79,79 @@ export default function Home() {
                 </div>
             </div>
             <div>
-                <form className="wrapper2" action="/action_page.php">
-                    <div style={{ display: "flex", flexDirection: "row" }}>
-                        <div for="nsurat" style={{ width: "120px" }}> Nomor Surat : </div>
-                        <input onChange={(res) => setNomorSurat(res.target.value)} type="text" id="nsurat" name="fname" value={nomorSurat} className="input" />
-                    </div>
-                    <div style={{ display: "flex", flexDirection: "row" }}>
-                        <div for="lname" style={{ width: "120px" }}> Sifat Surat : </div>
-                        <div style={{ display: "flex", gap: "20px" }}>
-                            <div>
-                                <input type="radio" onChange={(res) => setSifatSurat(res.target.value)} id="biasa" name="fav_language" value="true" />
-                                <label for="html"> Biasa </label>
-                            </div>
-                            <div>
-                                <input type="radio" onChange={(res) => setSifatSurat(res.target.value)} id="rahasia" name="fav_language" value="false" />
-                                <label for="css"> Rahasia </label>
+                <Transition appear show={isOpen} as={Fragment}>
+                    <Dialog as="div" className="relative z-10" onClose={closeModal}>
+                        <Transition.Child
+                            as={Fragment}
+                            enter="ease-out duration-300"
+                            enterFrom="opacity-0"
+                            enterTo="opacity-100"
+                            leave="ease-in duration-200"
+                            leaveFrom="opacity-100"
+                            leaveTo="opacity-0"
+                        >
+                            <div className="fixed inset-0 bg-black/25" />
+                        </Transition.Child>
+
+                        <div className="fixed inset-0 overflow-y-auto">
+                            <div className="flex min-h-full items-center justify-center p-4 text-center">
+                                <Transition.Child
+                                    as={Fragment}
+                                    enter="ease-out duration-300"
+                                    enterFrom="opacity-0 scale-95"
+                                    enterTo="opacity-100 scale-100"
+                                    leave="ease-in duration-200"
+                                    leaveFrom="opacity-100 scale-100"
+                                    leaveTo="opacity-0 scale-95">
+                                    <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+
+                                        <div>
+                                            <form className="wrapper2" action="/action_page.php">
+                                                <div style={{ display: "flex", flexDirection: "row" }}>
+                                                    <div for="nsurat" style={{ width: "120px" }}> Nomor Surat : </div>
+                                                    <input onChange={(res) => setNomorSurat(res.target.value)} type="text" id="nsurat" name="fname" value={nomorSurat} className="input" />
+                                                </div>
+                                                <div style={{ display: "flex", flexDirection: "row" }}>
+                                                    <div for="lname" style={{ width: "120px" }}> Sifat Surat : </div>
+                                                    <div style={{ display: "flex", gap: "20px" }}>
+                                                        <div>
+                                                            <input type="radio" onChange={(res) => setSifatSurat(res.target.value)} id="biasa" name="fav_language" value="true" />
+                                                            <label for="html"> Biasa </label>
+                                                        </div>
+                                                        <div>
+                                                            <input type="radio" onChange={(res) => setSifatSurat(res.target.value)} id="rahasia" name="fav_language" value="false" />
+                                                            <label for="css"> Rahasia </label>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div style={{ display: "flex", flexDirection: "row" }}>
+                                                    <div for="lname" style={{ width: "120px" }}> Perihal : </div>
+                                                    <input onChange={(res) => setPerihal(res.target.value)} type="text" id="lname" name="lname" value={perihal} className="input" />
+                                                </div>
+                                                <div style={{ display: "flex", flexDirection: "row" }}>
+                                                    <div for="dari" style={{ width: "120px" }}> Dari : </div>
+                                                    <input onChange={(res) => setDari(res.target.value)} type="text" id="dari" name="dari" value={dari} className="input" />
+                                                </div>
+                                                <div style={{ display: "flex", flexDirection: "row" }}>
+                                                    <div for="tujuan" style={{ width: "120px" }}> Tujuan : </div>
+                                                    <input onChange={(res) => setTujuan(res.target.value)} type="text" id="tujuan" name="tujuan" value={tujuan} className="input" />
+                                                </div>
+                                            </form>
+                                        </div>
+                                        <div className="mt-4">
+                                            <button
+                                                type="button"
+                                                className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                                                onClick={handleSubmit}>
+                                                Submit
+                                            </button>
+                                        </div>
+                                    </Dialog.Panel>
+                                </Transition.Child>
                             </div>
                         </div>
-                    </div>
-                    <div style={{ display: "flex", flexDirection: "row" }}>
-                        <div for="lname" style={{ width: "120px" }}> Perihal : </div>
-                        <input onChange={(res) => setPerihal(res.target.value)} type="text" id="lname" name="lname" value={perihal} className="input" />
-                    </div>
-                    <div style={{ display: "flex", flexDirection: "row" }}>
-                        <div for="dari" style={{ width: "120px" }}> Dari : </div>
-                        <input onChange={(res) => setDari(res.target.value)} type="text" id="dari" name="dari" value={dari} className="input" />
-                    </div>
-                    <div style={{ display: "flex", flexDirection: "row" }}>
-                        <div for="tujuan" style={{ width: "120px" }}> Tujuan : </div>
-                        <input onChange={(res) => setTujuan(res.target.value)} type="text" id="tujuan" name="tujuan" value={tujuan} className="input" />
-                    </div>
-                </form>
-                <div>
-                    <button className="submit" onClick={handleSubmit}>Submit</button>
-                </div>
+                    </Dialog>
+                </Transition>
             </div>
         </div>
     )
